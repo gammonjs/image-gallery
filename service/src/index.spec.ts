e93228288651d66d153472e41818a81949c58402
Joseph <gammon.joseph@gmail.com>
2021-07-31 18:59:50 -0500

    feat: service unit test scaffold

---
+++ service/src/index.spec.ts
@@ -0,0 +1,7 @@
+import { Run } from './index'
+
+describe('auth', () => {
+    it('should resolve with true and valid userId for hardcoded token', async () => {
+        expect(true).toEqual(true);
+    });
+});
